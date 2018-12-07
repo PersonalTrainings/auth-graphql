6f5760116f54d3829ac648855cc404dd709f3b41
Narek Khachatryan <narek1993x@mail.ru>
2018-12-07 12:33:20 +0400

    first commit

---
+++ client/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+
+import App from './components/App';
+import './index.styl';
+
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
+const client = new ApolloClient({
+  networkInterface,
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path='/' component={App} />
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
---
+++ server/server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./services/auth');
+const MongoStore = require('connect-mongo')(session);
+const config = require('./config/config.json');
+const schema = require('./schema/schema');
+
+// Create a new Express application
+const app = express();
+
+// Webpack runs as a middleware.  If any request comes in for the root route ('/')
+// Webpack will respond with the output of the webpack process: an HTML file and
+// a single bundle.js output of all of our client side Javascript
+(function() {
+  // Step 1: Create & configure a webpack compiler
+  const webpack = require('webpack');
+  const webpackConfig = require('../webpack.config');
+  const compiler = webpack(webpackConfig);
+
+  // Step 2: Attach the dev middleware to the compiler & the server
+  app.use(
+    require('webpack-dev-middleware')(compiler, {
+      logLevel: 'warn',
+      publicPath: '/',
+      watchOptions: {
+        poll: true
+      },
+      stats: {
+        colors: true
+      }
+    })
+  );
+
+  // Step 3: Attach the hot middleware to the compiler & the server
+  app.use(
+    require('webpack-hot-middleware')(compiler, {
+      log: console.log,
+      path: '/__webpack_hmr',
+      heartbeat: 10 * 1000
+    })
+  );
+})();
+
+// Local DB
+const MONGO_URI = `mongodb://${config.db.host}:${config.db.port}/${config.db.dbName}`;
+
+// Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
+mongoose.Promise = global.Promise;
+
+// Connect to the mongoDB instance and log a message
+// on success or failure
+mongoose.connect(MONGO_URI, { useMongoClient: true });
+mongoose.connection
+    .once('open', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+// Configures express to use sessions.  This places an encrypted identifier
+// on the users cookie.  When a user makes a request, this middleware examines
+// the cookie and modifies the request object to indicate which user made the request
+// The cookie itself only contains the id of a session; more data about the session
+// is stored inside of MongoDB.
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'aaabbbccc',
+  store: new MongoStore({
+    url: MONGO_URI,
+    autoReconnect: true
+  })
+}));
+
+// Passport is wired into express as a middleware. When a request comes in,
+// Passport will examine the request's session (as set by the above config) and
+// assign the current user to the 'req.user' object.  See also servces/auth.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Instruct Express to pass on any request made to the '/graphql' route
+// to the GraphQL instance.
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+module.exports = app;
